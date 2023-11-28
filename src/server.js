a6a187a3a1013c7957aa163fe3a2c1a5e2faa56a
Sergii <sergii.volodarskyi@gmail.com>
2023-11-28 14:24:13 -0700

    add node.js app code

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = redquire('express');
+const app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to my awesome app!');
+})
+
+app.listen(3000, function(){
+    console.log('app listening on port 3000');
+})
